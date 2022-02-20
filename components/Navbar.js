62af024de20ba299c3331cc6d57f5de380efdaad
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-02-20 15:02:26 +0530

    chore: updated socials tab to "my stuff"

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,11 @@
-import Link from "next/link";
 import NavButton from "./NavButton";
 export default function Navbar(){
     return(
         <div className="flex shadow-sm justify-evenly pb-3 pt-4 mb-1">
             <NavButton path="/" label="Home"/>
             <NavButton path="./projects" label="Projects"/>
-            <NavButton path="/" label="Blogs"/>
-            <NavButton path="/" label="Tech Socials"/>
-           
+            <NavButton path="./blogs" label="Blogs"/>
+            <NavButton path="./my_stuff" label="My stuff"/>
         </div>
     )
 }
