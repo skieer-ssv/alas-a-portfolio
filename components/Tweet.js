53528244c60f212335ac49c306db8d25eefb1742
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-02-26 16:31:30 +0530

    Revert "chore: shift hardcode to variables"
    This reverts commit 144e6b7fd885817022f3f91e2b8a1b5e13281456.

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,18 +1,22 @@
 import Head from 'next/head';
 import tweetIds from '../data/tweetList';
-
+// function TweetPlaceholder(){
+//     return(
+//         <div className="tweet-placeholder m-4">
+//             <div className="tweet-placeholder-text">
+//                 <p className='text-center'>If you finish reading this before a tweet loads<br/> either your net is slow or my code is shitty</p>
+//             </div>
+//         </div>
+//     )
+// }
 export default function Tweets(){
     
     const tweetIdsLength= tweetIds.length;
     const randomTweet=Math.floor(Math.random()*tweetIdsLength);
-    const placeholderText=`If you finish reading this before a tweet loads either your net is slow or my code is shitty`;
-    const tweetId=tweetIds[randomTweet];
-    const tweetUrl=`https://twitter.com/siddhant2102/status/${tweetId}`;
-   
     return(
         <div className="">
             <blockquote className="twitter-tweet" data-dnt="true" >
-                <p lang="en" dir="ltr">{placeholderText}</p><a href={tweetUrl}></a></blockquote>
+                <p lang="en" dir="ltr">If you finish reading this before a tweet loads either your net is slow or my code is shitty<a href="https://twitter.com/hashtag/NLP?src=hash&amp;ref_src=twsrc%5Etfw"></a></p><a href={`https://twitter.com/siddhant2102/status/${tweetIds[ randomTweet]}?ref_src=twsrc%5Etfw`}></a></blockquote>
                <Head>
                <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script></Head> 
             {/* <TwitterTweetEmbed tweetId={tweetIds[ randomTweet] } placeholder={TweetPlaceholder()} /> */}
