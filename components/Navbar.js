043a5da9f31dd80358b52e20c6f13b8c6241cfe9
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-02-28 21:28:59 +0530

    fix: different navbar paths for client and server

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,9 +3,9 @@ export default function Navbar(){
     return(
         <div className="flex shadow-md justify-evenly pb-3 pt-4 mb-1">
             <NavButton path="/" label="Home"/>
-            <NavButton path="./projects" label="Projects"/>
-            <NavButton path="./blogs" label="Blogs"/>
-            <NavButton path="./my_stuff" label="My stuff"/>
+            <NavButton path="/projects" label="Projects"/>
+            <NavButton path="/blogs" label="Blogs"/>
+            <NavButton path="/my_stuff" label="My stuff"/>
         </div>
     )
 }
