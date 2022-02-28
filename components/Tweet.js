556377ad4f24f966dcf1a865d8592daa52b539f7
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-02-28 21:29:31 +0530

    chore: dnt remmeber

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,22 +1,21 @@
 import Head from 'next/head';
 import tweetIds from '../data/tweetList';
-// function TweetPlaceholder(){
-//     return(
-//         <div className="tweet-placeholder m-4">
-//             <div className="tweet-placeholder-text">
-//                 <p className='text-center'>If you finish reading this before a tweet loads<br/> either your net is slow or my code is shitty</p>
-//             </div>
-//         </div>
-//     )
-// }
+import {useState,useEffect} from 'react';
 export default function Tweets(){
-    
+    const [tweetId,setTweetId]=useState([]);    
     const tweetIdsLength= tweetIds.length;
     const randomTweet=Math.floor(Math.random()*tweetIdsLength);
+    useEffect(()=>{
+        setTweetId(tweetIds[randomTweet]);
+    }, [randomTweet]);
     return(
         <div className="">
             <blockquote className="twitter-tweet" data-dnt="true" >
-                <p lang="en" dir="ltr">If you finish reading this before a tweet loads either your net is slow or my code is shitty<a href="https://twitter.com/hashtag/NLP?src=hash&amp;ref_src=twsrc%5Etfw"></a></p><a href={`https://twitter.com/siddhant2102/status/${tweetIds[ randomTweet]}?ref_src=twsrc%5Etfw`}></a></blockquote>
+                <p lang="en" dir="ltr">
+                   
+                    If  you can finish reading this sentence before a tweet loads either your net is super slow or my code is shitty.
+                    </p>
+                    <a href={`https://twitter.com/siddhant2102/status/${tweetId}?ref_src=twsrc%5Etfw`}></a></blockquote>
                <Head>
                <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script></Head> 
             {/* <TwitterTweetEmbed tweetId={tweetIds[ randomTweet] } placeholder={TweetPlaceholder()} /> */}
